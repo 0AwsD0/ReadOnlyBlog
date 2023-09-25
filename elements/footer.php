41ea07dcff4ce692a13ad35c1f8613ae29affc2a
0AwsD0 <64651813+0AwsD0@users.noreply.github.com>
2023-09-25 23:18:34 +0200

    bugfix #2 ???

--- elements/footer.php
+++ elements/footer.php
@@ -1,11 +1,6 @@
 <div class="row footer_links justify-content-md-center">
             <div class="col-auto">
-                   Links and icons to csocial media gnerated by php db?.
-<!--
-Linki w bazie danych z ikonkami i jeżeli active to git //loista nmajbardziej popoularnych mediow i ikonek w bazie ..//db row ": boolan active true false
-(kod ikonek jako txt z html icon link to np. fontawesome albo local)
-- lista - FB, Insta, LinkedIn, Youtube, Twitch, TikTok, Patreon, Cofee, Mail, Twitter (X), Snapchat
--->
+                   <img src="img/ico/link.png" alt="[Link] " class="footer_link_image"> <a href="#" target="_blank" class="footer_link_text">Link to display</a>
             </div>
 </div>
 <div class="row footer justify-content-md-center">
